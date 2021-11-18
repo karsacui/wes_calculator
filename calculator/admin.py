850115b74b2f7ff86d1105e502409ef44df52e22
Zhengyang Cui <czy77jsy@gmail.com>
2021-11-18 12:55:05 +0800

    finish models

--- calculator/admin.py
+++ calculator/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Student, Course
 
-admin.site.register(Student)
-admin.site.register(Course)
+
+# admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_wes_gpa', 'get_njupt_gpa', )
+
+
+# admin.site.register(Course)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'semester', 'credit', 'get_wes_credit', 'get_wes_gp', 'pk',)
--- calculator/models.py
+++ calculator/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.db.models.query_utils import select_related_descend
 from django.utils.translation import gettext_lazy as _
+
 
 class Student(models.Model):
     name = models.CharField(max_length=10)
@@ -8,18 +8,45 @@ class Student(models.Model):
     def __str__(self):
         return self.name
 
-    def get_gpa(self):
-        pass
+    def get_wes_gpa(self):
+        queryset = Course.objects.filter(student=self)
+        gp_sum = 0
+        credit_sum = 0
+        test = ''
+        for course in queryset:
+            credit = course.get_wes_credit()
+            gp_sum += course.get_wes_gp()*credit
+            credit_sum += credit
+            test += str(credit) + "*" + str(course.get_wes_gp()) + "+"
+        print(test)
+        print(gp_sum)
+        print(credit_sum)
+        return gp_sum / credit_sum
+
+    def get_njupt_gpa(self):
+        queryset = Course.objects.filter(student=self)
+        gp_sum = 0
+        credit_sum = 0
+        for course in queryset:
+            if course.type == 'RX':
+                continue
+            gp_sum += course.get_njupt_gp()*course.credit
+            credit_sum += course.credit
+        return gp_sum / credit_sum
+
 
 class Course(models.Model):
-
     def __str__(self):
-        pass
+        if self.grade:
+            return str(self.student)+self.name+self.grade
+        else:
+            return str(self.student)+self.name+str(self.score)
 
     class CourseType(models.TextChoices):
         BIXIU = 'BX', _('必修')
         XIANXUAN = 'XX', _('限选')
         RENXUAN = 'RX', _('任选')
+
     class Grade(models.TextChoices):
         A = 'A', _('优秀')
         B = 'B', _('良好')
@@ -38,16 +65,22 @@ class Course(models.Model):
     grade = models.CharField(max_length=1, choices=Grade.choices, default='', null=True, blank=True)
     student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True)
 
+    def get_wes_credit(self):
+        if self.override_credit == None:
+            return self.credit
+        else:
+            return self.override_credit
+
     def get_njupt_gp(self):
-        if self.grade != '':
-            grade_gp_dict = {
-                'A': 4.5,
-                'B': 3.5,
-                'C': 2.5,
-                'D': 1.5,
-                'F': 0,
-                'P': 1.1,
-            }
+        grade_gp_dict = {
+            'A': 4.5,
+            'B': 3.5,
+            'C': 2.5,
+            'D': 1.5,
+            'F': 0,
+            'P': 1.1,
+        }
+        if self.grade in grade_gp_dict.keys():
             return grade_gp_dict[self.grade]
         else:
             if self.score >= 60:
@@ -56,15 +89,23 @@ class Course(models.Model):
                 return 0
 
     def get_wes_gp(self):
-        if self.grade != '':
-            grade_gp_dict = {
-                'A': 4,
-                'B': 3,
-                'C': 2,
-                'D': 1,
-                'F': 0,
-                'P': 2,
-            }
+        grade_gp_dict = {
+            'A': 4,
+            'B': 3,
+            'C': 2,
+            'D': 1,
+            'F': 0,
+            'P': 2,
+        }
+        if self.grade in grade_gp_dict.keys():
             return grade_gp_dict[self.grade]
         else:
-            
+            if self.score >= 85:
+                return 4
+            elif self.score >= 75:
+                return 3
+            elif self.score >= 60:
+                return 2
+            else:
+                return 1
+
